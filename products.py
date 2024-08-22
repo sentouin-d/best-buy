4a8dad43762317f5d35b6ef63ec23d4b452b2c2a
sentouin-d <gnerkus@protonmail.com>
2024-08-22 07:37:30 +0000

    define class for store product

---
+++ products.py
@@ -0,0 +1,37 @@
+class Product:
+    def __init__(self, name="", price=0.0, quantity=1) -> None:
+        if (len(name) < 1):
+            raise ValueError("Empty name")
+
+        self.quantity = quantity
+        self.name = name
+        self.price = price
+        self.active = True
+
+    def __del__(self) -> None:
+        pass
+
+    def get_quantity(self) -> float:
+        return self.quantity
+    
+    def set_quantity(self, quantity) -> None:
+        self.quantity = quantity
+
+    def is_active(self) -> bool:
+        return self.active
+    
+    def activate(self) -> None:
+        self.active = True
+
+    def deactivate(self) -> None:
+        self.active = False
+
+    def show(self) -> str:
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+    
+    def buy(self, quantity) -> float:
+        if (quantity > self.quantity):
+            raise ValueError("Not enough items in stock")
+        
+        self.quantity = max(self.quantity - quantity, 0)
+        return self.price * quantity
